02243d0931f03221250a59b78d2550e6afb4b323
phaANTom2303 <anikavy.goenka@gmail.com>
2025-09-19 00:54:29 +0530

    better project data fetch

--- server/routes/projectRouter.js
+++ server/routes/projectRouter.js
@@ -2,9 +2,22 @@ const express = require('express');
 const { Project } = require('../models');
 const projectRouter = express.Router();
 
-projectRouter.get('/', async (req, res) => {
-    const fetchedProjects = await Project.find({});
-    return res.status(200).json(fetchedProjects);
+projectRouter.get('/', async (req, res, next) => {
+    try {
+        // Populate only the 'name' field, then convert to plain objects and replace with string
+        const projects = await Project.find({})
+            .populate({ path: 'organization', select: 'name -_id' })
+            .lean();
+
+        const shaped = projects.map(p => ({
+            ...p,
+            organization: p.organization?.name || null
+        }));
+
+        return res.status(200).json(shaped);
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = projectRouter;
