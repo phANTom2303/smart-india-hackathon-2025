29311c5703a420082c2a5cd2399d1d79692f7505
phaANTom2303 <anikavy.goenka@gmail.com>
2025-09-19 01:04:21 +0530

    minor fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors'); 2
+const cors = require('cors');
 const app = express();
 
 require('dotenv').config();
