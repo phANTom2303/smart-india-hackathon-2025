9dcfc26aa42cc9d315fac7e2ec42ed82dfefc247
jaspreetjk20 <jaspreetsk.2020@gmail.com>
2025-09-19 18:35:43 +0530

    Report functions completed

---
+++ client/src/Test.jsx
@@ -0,0 +1,9 @@
+//import AdminOverview from "./adminComponents/Overview/AdminOverview";
+//import ReportEdit from "./adminComponents/ReportEdit/ReportEdit";
+import ReportAction from "./adminComponents/ReportAction/ReportAction";
+export default function Test() {
+    return <>
+     <ReportAction></ReportAction>
+        
+    </>
+}
